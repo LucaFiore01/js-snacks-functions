1d1a19851494602620a607c35f68381316f72d34
Luca Fiore <lf44551@gmail.com>
2025-12-09 14:56:03 +0100

    Completato l'esercizio numero uno

---
+++ 01_saluti/script.js
@@ -0,0 +1,21 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Luca';
+
+
+// Dichiara la funzione qui.
+function greet(name) {
+    return 'Ciao ' + name;
+}
+
+console.log(greet(userName)); 
+
+// Invoca la funzione qui e stampa il risultato in console.
+function greet(name) {
+    return 'Ciao ' + name;
+}
+console.log(greet(userName));
+
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
