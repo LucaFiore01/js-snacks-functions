f4ec8a284edbb42b7508531b8f128081fd1bd4ee
Luca Fiore <lf44551@gmail.com>
2025-12-09 16:50:11 +0100

    completato il secondo esercizio

---
+++ 02_iniziali/script.js
@@ -0,0 +1,30 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+function getInitials(nameArray) {
+    const initialsArray = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        const name = nameArray[i];
+        const initial = name.charAt(0);
+        initialsArray.push(initial);
+    }
+    return initialsArray;
+}
+
+
+
+
+// Invoca la funzione qui e stampa il risultato in console.
+
+getInitials(names);
+
+console.log(getInitials(names));
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
